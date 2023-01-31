9023fdbaa1f2170d31a233c60550ab800e926ff6
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-01-31 12:51:40 +0530

    Add: Flipping coin to determine the percentage of singlet

--- FlipCoinCombinationProblem.sh
+++ FlipCoinCombinationProblem.sh
@@ -1,9 +1,29 @@
 echo "Welcome to Flip Coin Combination Problem"
 
-flip=$(( RANDOM%2 ))
-if(( flip == 0 ))
-then
-    echo "Heads"
-else
-    echo "Tails"
-fi
+read -p "Enter the number of times to flip the singlet:" num
+declare -A singletFlip
+
+singletFlip[H]=0
+singletFlip[T]=0
+
+for(( flip = 1; flip <= num; flip++ ))
+do
+    echo -n "Flip number $flip is "
+    (( count = RANDOM%2 ))
+    case $count in
+        0)
+            echo "Heads"
+            (( singletFlip[H]++ ))
+            ;;
+        1)
+            echo "Tails"
+            (( singletFlip[T]++ ))
+    esac
+done
+
+for combination in ${!singletFlip[@]}
+do
+    percent=$(( ${singletFlip[$combination]}  * 100 / num ))
+    singletFlip[$combination]=$percent
+    echo "The percentage of $combination is ${singletFlip[$combination]}%"
+done
